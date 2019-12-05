7c7f822c83fb04a4531aeccbc6775912020965da
James Santucci <james.santucci@gmail.com>
2019-12-04 20:52:10 -0600

    Solve day 3 part 1

--- 2019/day3/app/Main.hs
+++ 2019/day3/app/Main.hs
@@ -1,13 +1,31 @@
 module Main where
 
-import           Control.Monad.Trans.State
-import           Data.Sequence             (Seq (..))
+import           Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString                  as BS
+import           Data.Foldable
+import           Data.Sequence
+import qualified Data.Set                         as Set
 import           Lib
 
-instructions :: [Instruction]
-instructions =
-  [Right' 4, Left' 4]
+toSet :: Ord a => Seq a -> Set.Set a
+toSet Empty      = Set.empty
+toSet (x :<| xs) = Set.singleton x `Set.union` toSet xs
 
 main :: IO ()
-main =
-  print . show . snd $ interpretInstructions instructions Empty
+main = do
+  inText <- BS.readFile "puzzle.txt"
+  let parsed = parseOnly (many' puzzleLineParser) inText
+  case parsed of
+    Left err -> error err
+    Right wires ->
+      let histories = interpretInstructions Empty <$> wires
+          pointSets = toSet <$> histories
+          sharedPoints = foldl' Set.intersection (head pointSets) pointSets
+       in print . show $
+          Set.foldl'
+            (\x y ->
+               if (manhattanDistance x < manhattanDistance y && x /= (0, 0))
+                 then x
+                 else y)
+            (0, 0)
+            sharedPoints
--- 2019/day3/src/Lib.hs
+++ 2019/day3/src/Lib.hs
@@ -1,16 +1,16 @@
 module Lib
   ( Instruction(..)
   , interpretInstructions
+  , manhattanDistance
+  , puzzleLineParser
   ) where
 
 import           Control.Applicative              ((<|>))
-import           Control.Monad.Trans.State
 import           Data.Attoparsec.ByteString.Char8
 import           Data.Sequence
 
-import           Debug.Trace                      (trace)
-
 type Point = (Int, Int)
+
 type History = Seq Point
 
 data Instruction
@@ -22,29 +22,30 @@ data Instruction
 getParser :: (Int -> Instruction) -> Char -> Parser Instruction
 getParser f c = f <$> (char c *> decimal)
 
-parseInstruction :: Parser Instruction
-parseInstruction = do
+instParser :: Parser Instruction
+instParser = do
   getParser Right' 'R' <|> getParser Left' 'L' <|> getParser Up 'U' <|>
     getParser Down 'D'
 
-interpretInstructions :: [Instruction] -> History -> ([Instruction], History)
-interpretInstructions instrs Empty = interpretInstructions instrs (singleton (0, 0))
-interpretInstructions [] hist = ([], hist)
-interpretInstructions (inst:instrs) hist@(_ :|> (x, y)) =
+puzzleLineParser :: Parser [Instruction]
+puzzleLineParser = sepBy instParser (char ',') <* endOfLine
+
+interpretInstructions :: History -> [Instruction] -> History
+interpretInstructions Empty instrs =
+  interpretInstructions (singleton (0, 0)) instrs
+interpretInstructions hist [] = hist
+interpretInstructions hist@(_ :|> (x, y)) (inst:instrs) =
   case inst of
-    Right' 0 ->
-      interpretInstructions instrs hist
-    Left' 0 ->
-      interpretInstructions instrs hist
-    Up 0 ->
-      interpretInstructions instrs hist
-    Down 0 ->
-      interpretInstructions instrs hist
+    Right' 0 -> interpretInstructions hist instrs
+    Left' 0 -> interpretInstructions hist instrs
+    Up 0 -> interpretInstructions hist instrs
+    Down 0 -> interpretInstructions hist instrs
     Right' n ->
-      interpretInstructions (Right' (n - 1) : instrs) (hist |> (x + 1, y))
+      interpretInstructions (hist |> (x + 1, y)) (Right' (n - 1) : instrs)
     Left' n ->
-      interpretInstructions (Left' (n - 1) : instrs) (hist |> (x - 1, y))
-    Up n ->
-      interpretInstructions (Up (n - 1) : instrs) (hist |> (x, y + 1))
-    Down n ->
-      interpretInstructions (Down (n - 1) : instrs) (hist |> (x, y - 1))
+      interpretInstructions (hist |> (x - 1, y)) (Left' (n - 1) : instrs)
+    Up n -> interpretInstructions (hist |> (x, y + 1)) (Up (n - 1) : instrs)
+    Down n -> interpretInstructions (hist |> (x, y - 1)) (Down (n - 1) : instrs)
+
+manhattanDistance :: Point -> Int
+manhattanDistance (x, y) = abs x + abs y
