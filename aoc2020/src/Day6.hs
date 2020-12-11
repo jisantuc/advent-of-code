38abe16e07d382efaa838cc6b93debde1019e684
James Santucci <james.santucci@gmail.com>
2020-12-10 19:20:09 -0700

    day6 solution part 2

--- aoc2020/src/Day6.hs
+++ aoc2020/src/Day6.hs
@@ -1,5 +1,6 @@
 module Day6 where
 
+import Data.List (foldl')
 import qualified Data.Set as Set
 import Parser (Parser)
 import Text.Megaparsec (sepBy1, sepEndBy, some)
@@ -9,7 +10,8 @@ type AnswerGroup = Set.Set Char
 
 answerGroupParser :: Parser AnswerGroup
 answerGroupParser =
-  mconcat <$> sepEndBy (Set.fromList <$> some alphaNumChar) eol
+  let allLetters = Set.fromList ['a' .. 'z']
+   in foldl' (\s1 s2 -> Set.intersection s1 s2) allLetters <$> sepEndBy (Set.fromList <$> some alphaNumChar) eol
 
 puzzleParser :: Parser [AnswerGroup]
 puzzleParser = sepBy1 answerGroupParser eol
