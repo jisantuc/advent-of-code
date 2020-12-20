e835da516ac35a4276d3424a94260d391a0e6dd4
James Santucci <james.santucci@gmail.com>
2020-12-19 19:23:03 -0700

    day12 solution part 2
    everything was converted to doubles so I could use
    sin / cos for rotation, which worked really well except
    for all the time I spent with the wrong sign for the new
    Y component 😱

--- aoc2020/src/Day12.hs
+++ aoc2020/src/Day12.hs
@@ -8,101 +8,99 @@ import Text.Megaparsec (sepEndBy, (<|>))
 import Text.Megaparsec.Char (char, eol, string)
 import Text.Megaparsec.Char.Lexer (decimal)
 
-data MoveDirection = North Int | South Int | East Int | West Int | Forward Int deriving (Eq, Show)
+data MoveWaypoint
+  = North Int
+  | South Int
+  | East Int
+  | West Int
+  deriving (Eq, Show)
+
+type FullSteamAhead = Int
 
 data CardinalDirection = N | S | E | W deriving (Eq, Show)
 
-data Turn = TurnLeft | TurnRight | TurnAround deriving (Eq, Show)
-
-data TurnType = Ninety | OneEighty | TwoSeventy deriving (Eq, Show)
+data Rotation = TurnRight Double | TurnLeft Double deriving (Eq, Show)
 
 data ShipPosition = ShipPosition
-  { facing :: CardinalDirection,
-    location :: (Int, Int)
+  { waypointVector :: (Double, Double),
+    location :: (Double, Double)
   }
-  deriving (Show)
-
-data Instruction
-  = Move MoveDirection
-  | Steer Turn
   deriving (Eq, Show)
 
-turnParser :: Parser Turn
-turnParser = do
-  direction <- TurnLeft <$ char 'L' <|> TurnRight <$ char 'R'
-  degrees <- Ninety <$ string "90" <|> OneEighty <$ string "180" <|> TwoSeventy <$ string "270"
-  pure $ case degrees of
-    Ninety -> direction
-    OneEighty -> TurnAround
-    TwoSeventy -> case direction of
-      TurnRight -> TurnLeft
-      TurnLeft -> TurnRight
-      turn -> turn
+data Instruction
+  = MoveWayPoint MoveWaypoint
+  | MoveShip FullSteamAhead
+  | RotateWaypoint Rotation
+  deriving (Eq, Show)
 
-moveParser :: (Int -> MoveDirection) -> Char -> Parser MoveDirection
+toRadians :: Int -> Double
+toRadians degrees = (fromIntegral degrees) / 360 * (2 * pi)
+
+rotationParser :: Parser Rotation
+rotationParser =
+  do
+    TurnLeft <$> (char 'L' *> (toRadians <$> decimal))
+    <|> TurnRight <$> (char 'R' *> (toRadians <$> decimal))
+
+moveParser :: (Int -> MoveWaypoint) -> Char -> Parser MoveWaypoint
 moveParser f c =
   char c *> (f <$> decimal)
 
+goForwardParser :: Parser FullSteamAhead
+goForwardParser = char 'F' *> decimal
+
 instructionParser :: Parser Instruction
 instructionParser =
-  Steer <$> turnParser
-    <|> Move
+  RotateWaypoint <$> rotationParser
+    <|> MoveWayPoint
       <$> ( moveParser North 'N'
               <|> moveParser East 'E'
               <|> moveParser West 'W'
               <|> moveParser South 'S'
-              <|> moveParser Forward 'F'
           )
+    <|> MoveShip <$> goForwardParser
 
 puzzleParser :: Parser [Instruction]
 puzzleParser = sepEndBy instructionParser eol
 
-left :: CardinalDirection -> CardinalDirection
-left N = W
-left W = S
-left S = E
-left E = N
+moveWaypoint :: ShipPosition -> MoveWaypoint -> ShipPosition
+moveWaypoint position@(ShipPosition (x, y) _) (East n) =
+  position {waypointVector = (x + fromIntegral n, y)}
+moveWaypoint position@(ShipPosition (x, y) _) (West n) =
+  position {waypointVector = (x - fromIntegral n, y)}
+moveWaypoint position@(ShipPosition (x, y) _) (North n) =
+  position {waypointVector = (x, y + fromIntegral n)}
+moveWaypoint position@(ShipPosition (x, y) _) (South n) =
+  position {waypointVector = (x, y - fromIntegral n)}
 
-right :: CardinalDirection -> CardinalDirection
-right N = E
-right E = S
-right S = W
-right W = N
+rotateWaypoint :: ShipPosition -> Rotation -> ShipPosition
+rotateWaypoint position@(ShipPosition (wpX, wpY) _) rotation =
+  let rotationAngle =
+        ( case rotation of
+            TurnRight radians -> (2 * pi) - radians
+            TurnLeft radians -> radians
+        )
+      rotatedX = wpX * cos rotationAngle - wpY * sin rotationAngle
+      rotatedY = wpX * sin rotationAngle + wpY * cos rotationAngle
+   in position
+        { waypointVector =
+            ( rotatedX,
+              rotatedY
+            )
+        }
 
-aboutFace :: CardinalDirection -> CardinalDirection
-aboutFace N = S
-aboutFace S = N
-aboutFace E = W
-aboutFace W = E
-
-moveShip :: ShipPosition -> MoveDirection -> ShipPosition
-moveShip (ShipPosition face (x, y)) (Forward n) =
-  case face of
-    E -> ShipPosition face (x + n, y)
-    W -> ShipPosition face (x - n, y)
-    N -> ShipPosition face (x, y + n)
-    S -> ShipPosition face (x, y - n)
-moveShip position@(ShipPosition _ (x, y)) (East n) =
-  position {location = (x + n, y)}
-moveShip position@(ShipPosition _ (x, y)) (West n) =
-  position {location = (x - n, y)}
-moveShip position@(ShipPosition _ (x, y)) (North n) =
-  position {location = (x, y + n)}
-moveShip position@(ShipPosition _ (x, y)) (South n) =
-  position {location = (x, y - n)}
-
-steerShip :: ShipPosition -> Turn -> ShipPosition
-steerShip position@(ShipPosition facing _) turn =
-  case turn of
-    TurnRight -> position {facing = right facing}
-    TurnLeft -> position {facing = left facing}
-    TurnAround -> position {facing = aboutFace facing}
+moveShip :: ShipPosition -> Int -> ShipPosition
+moveShip (ShipPosition waypoint@(wpX, wpY) (shipX, shipY)) n =
+  let newShipX = shipX + wpX * fromIntegral n
+      newShipY = shipY + wpY * fromIntegral n
+   in ShipPosition waypoint (newShipX, newShipY)
 
 step :: ShipPosition -> Instruction -> ShipPosition
-step pos (Steer inst) = steerShip pos inst
-step pos (Move inst) = moveShip pos inst
+step pos (RotateWaypoint inst) = rotateWaypoint pos inst
+step pos (MoveShip inst) = moveShip pos inst
+step pos (MoveWayPoint inst) = moveWaypoint pos inst
 
 solve :: [Instruction] -> ShipPosition
 solve instructions =
-  let initial = ShipPosition E (0, 0)
+  let initial = ShipPosition (10, 1) (0, 0)
    in foldl' (\pos inst -> step pos inst) initial instructions
