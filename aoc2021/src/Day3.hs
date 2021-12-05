0e6fc9ac1074777f228967a776bc03d8cf556a0f
James <james.santucci@gmail.com>
2021-12-05 08:25:36 -0700

    Handle bitstrings of arbitrary length

--- aoc2021/src/Day3.hs
+++ aoc2021/src/Day3.hs
@@ -9,6 +9,17 @@ import Data.Foldable (foldl')
 
 type Puzzle = [Int]
 
+-- Sad because all the coercions were getting messy :(
+sadExp :: Int -> Int -> Int
+sadExp _ 0 = 1
+sadExp b pow = b * sadExp b (pow - 1)
+
+intLog :: Int -> Int
+intLog x
+  | x <= 1 = 0
+  | otherwise = go 1 x
+    where go n v = if sadExp 2 n > v then n - 1 else go (n + 1) x
+
 puzzleParser :: Parser Puzzle
 puzzleParser = binary `sepEndBy` eol
 
@@ -48,7 +59,12 @@ solvePart1 :: Puzzle -> Int
 solvePart1 [] = 1
 solvePart1 ns =
     let
-        gammaBits = findGammaBit ns <$> reverse [0 .. 4]
+        -- need a value with the most significant bit set
+        -- we don't know in advance what that bit is likely to be, but any value
+        -- with the most significant bit set is going to be greater than values
+        -- without that bit set
+        maxValue = maximum ns
+        gammaBits = findGammaBit ns <$> reverse [0 .. intLog maxValue]
         epsilonBits = invertBit <$> gammaBits
         gammaSum = bitSum gammaBits
         epsilonSum = bitSum epsilonBits
