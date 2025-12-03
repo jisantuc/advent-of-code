b59cb7f8d270992c091b7fdeaac959b30298eb53
James Santucci <james.santucci@gmail.com>
2025-12-02 23:08:34 -0800

    WIP -- 😴

--- aoc2025/src/Day3.hs
+++ aoc2025/src/Day3.hs
@@ -5,6 +5,7 @@ module Day3 (puzzleParser, solve1) where
 import AoC.Parser (Parser)
 import qualified AoC.Parser as Parser
 import Data.Foldable (Foldable (foldMap'), foldl')
+import Data.List (findIndex, tails)
 import qualified Data.Map.Strict as Map
 import Data.Monoid (Sum (..))
 import qualified Data.Text as Text
@@ -18,21 +19,27 @@ puzzleParser =
           (Map.fromList (zip (Text.pack . show <$> [0 .. 9]) [0 .. 9]))
    in many digits `sepEndBy` eol
 
-maxJoltage :: [Int] -> Int
-maxJoltage bank =
-  (\(x, y) -> read $ show x <> show y)
+maxJoltage :: Int -> [Int] -> Int
+maxJoltage batteriesToUse bank =
+  (read . foldMap' show)
     . foldl'
-      ( \acc@(r1, r2) (j, next) ->
+      ( \acc (j, next) ->
           case next of
-            -- when there's no next value, only the right digit can change
-            Nothing -> if j > r2 then (r1, j) else acc
-            -- when there is a next value, it's guaranteed >= 0, so
-            -- (j, 0) is a safe replacement if j is bigger than the lead digit,
-            -- and if j is bigger than the second digit, (r1, j) is correct
-            Just _ -> if j > r1 then (j, 0) else if j > r2 then (r1, j) else acc
+            xs ->
+              if length xs + 1 >= batteriesToUse
+                -- when there's _more than batteriesToUse digits remaining_ (including j),
+                -- the digit can go in the first slot that it's greater than,
+                -- and all remaining digits are zeroed.
+                then case findIndex (j >) acc of
+                  Just idx -> take idx acc <> [j] <> replicate (batteriesToUse - idx - 1) 0
+                  Nothing -> acc
+                -- when there's _less than or equal to batteriesToUse digits remaining_ (including j),
+                -- I think I have to treat the head of acc as fixed, and find the place to insert the
+                -- value in the tail, but I'm sleepy and I can't work that out right now
+                else undefined
       )
-      (0, 0)
-    $ zip bank ((Just <$> drop 1 bank) <> [Nothing])
+      (replicate batteriesToUse 0)
+    $ zip bank (drop 1 $ tails bank)
 
 solve1 :: [[Int]] -> Int
-solve1 = getSum . foldMap' (Sum . maxJoltage)
+solve1 = getSum . foldMap' (Sum . maxJoltage 2)
